98d389b3d7849c45207b525a0aa5b10e96f00ed8
Phillipip <info@der-dirigent.de>
2025-03-07 22:23:15 +0100

    Update match_value.sql

--- match_value.sql
+++ match_value.sql
@@ -1,29 +1,25 @@
 DROP FUNCTION IF EXISTS match_value;
 DELIMITER $$
-CREATE FUNCTION match_value(candidate INT, cond JSON) RETURNS BOOLEAN
+CREATE FUNCTION match_value(
+  candidate INT,
+  op VARCHAR(10),
+  eq_val INT,
+  between_start INT,
+  between_end INT,
+  in_list TEXT
+) RETURNS BOOLEAN
 DETERMINISTIC
 BEGIN
-    DECLARE op VARCHAR(20);
-    DECLARE val0 INT;
-    DECLARE val1 INT;
-    SET op = JSON_UNQUOTE(JSON_EXTRACT(cond, '$.op'));
-    
-    IF op = 'ALL' THEN
-        RETURN TRUE;
-    ELSEIF op = '=' THEN
-        SET val0 = CAST(JSON_UNQUOTE(JSON_EXTRACT(cond, '$.val')) AS UNSIGNED);
-        RETURN candidate = val0;
-    ELSEIF op = 'BETWEEN' THEN
-        SET val0 = CAST(JSON_UNQUOTE(JSON_EXTRACT(cond, '$.val[0]')) AS UNSIGNED);
-        SET val1 = CAST(JSON_UNQUOTE(JSON_EXTRACT(cond, '$.val[1]')) AS UNSIGNED);
-        RETURN candidate BETWEEN val0 AND val1;
-    ELSEIF op = 'IN' THEN
-        RETURN FIND_IN_SET(candidate, JSON_UNQUOTE(JSON_EXTRACT(cond, '$.val'))) > 0;
-    ELSE
-        RETURN FALSE;
-    END IF;
+  IF op = 'ALL' THEN
+    RETURN TRUE;
+  ELSEIF op = '=' THEN
+    RETURN candidate = eq_val;
+  ELSEIF op = 'BETWEEN' THEN
+    RETURN candidate BETWEEN between_start AND between_end;
+  ELSEIF op = 'IN' THEN
+    RETURN FIND_IN_SET(candidate, in_list) > 0;
+  ELSE
+    RETURN FALSE;
+  END IF;
 END$$
 DELIMITER ;
-
-DROP FUNCTION IF EXISTS get_next_execution;
-DELIMITER $$
